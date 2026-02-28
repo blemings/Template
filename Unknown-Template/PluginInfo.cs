02b13cfae7313701f5dbab5e6a6613a6e4555f2d
Unknown <156071321+blemings@users.noreply.github.com>
2026-02-28 21:12:40 +0300

    Update PluginInfo with new template details

--- Unknown-Template/PluginInfo.cs
+++ Unknown-Template/PluginInfo.cs
@@ -2,9 +2,9 @@
 {
     public class PluginInfo
     {
-        public const string GUID = "org.iidk.gorillatag.menutemplate";
-        public const string Name = "ii's Stupid Template";
-        public const string Description = "Created by @goldentrophy with love <3";
-        public const string Version = "1.9.8";
+        public const string GUID = "org.gorillatag.menutemplate";
+        public const string Name = "Unknown's Template";
+        public const string Description = "Created by @Unknown with hate <3";
+        public const string Version = "1.0.0";
     }
 }
